38784c557f5c457cdd745cfa536f111c9b13d921
Mayank <mayank.codenote@gmail.com>
2023-08-24 19:04:13 +0530

    created action in existing controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   #root 'articles#index'
   root 'sample#index'
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles
 end
